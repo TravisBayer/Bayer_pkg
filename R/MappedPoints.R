9bd05a988d4e34a92406ff6b9901874b596e05b0
Travis Bayer <travis.bayer@selu.edu>
2022-11-22 21:48:46 +0000

    Final two functions for package

--- R/MappedPoints.R
+++ R/MappedPoints.R
@@ -1,1 +1,21 @@
-#' Function to map data points on map with extra info when individual points are hovered over with cursor.
+#' Function to map data points of latitude and longitude from coordinates obtained from collected GPS data.
+
+#' @param filename name of file
+#'
+#' @return Mapped data points of collected GPS coordinates of bats in Panama.
+#' 
+#' @examples 
+#' 
+#' Mapping(batcoord)
+#' 
+#' @export
+#'
+library(tidyverse)
+library(sf)
+library(mapview)
+
+batcoord <- read_csv("/cloud/project/data/BatGPS.csv")
+
+Mapping <- function(dataframe){
+  mapview(batcoord, xcol = "Longitude", ycol = "Latitude", crs = 4269, grid = FALSE)
+}
---
+++ R/PowerPlot.R
@@ -0,0 +1,17 @@
+#' Function to calculate energy exerted compared to heart rate.
+
+#' @param filename name of file
+#'
+#' @return Line graph of selected data set comparing heart rate against energy being expended.
+#' 
+#' @examples 
+#' 
+#' Joules(accdata)
+#' 
+#' @export
+#'
+Joules <- function(dataframe){
+  heartjoules <- ggplot() +
+    geom_line(mapping = aes(x = fH.bpm, y = Joules.p.h), data = BatData)
+  return(heartjoules)
+}
